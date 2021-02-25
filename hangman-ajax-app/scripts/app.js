e2e34680bc9dd07518c5b63bef803714923a07e6
MinJeung-Kim <focso5@knou.ac.kr>
2021-02-25 14:17:05 +0900

    add puzzle change '*'

--- hangman-ajax-app/scripts/app.js
+++ hangman-ajax-app/scripts/app.js
@@ -2,18 +2,19 @@
 const puzzleDiv = document.querySelector('#puzzle');
 const guesses = document.querySelector('#guesses');
 let puzzleCount;
-function puzzleWord() {}
 
 const startGame = async () => {
   const puzzle = await getPuzzle('3');
 
-  // 추가 개발
   puzzleCount = puzzle.length;
 
   for (let i = 0; i < puzzleCount; i++) {
     console.log(`puzzle => ${puzzle.charAt(i)}`);
     const p = document.createElement('span');
-    p.innerText = puzzle.charAt(i);
+    if (puzzle.charAt(i) === null || puzzle.charAt(i) === ' ') {
+      p.innerText = ' ';
+    }
+    p.innerText = '*';
     puzzleDiv.appendChild(p);
   }
 };
