7ad083f9ba33a431b7b2565f33281ed3f87fa87c
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-01 16:39:37 +0900

    Matching puzzles by number

--- hangman-ajax-app/scripts/app.js
+++ hangman-ajax-app/scripts/app.js
@@ -2,43 +2,43 @@
 const puzzleDiv = document.querySelector('#puzzle');
 const guesses = document.querySelector('#guesses');
 
+let pushCount = 5;
 // 1. PUZZLE 가져오기 (서버연결)
 const startGame = async () => {
-  const puzzle = await getPuzzle('3');
+  const puzzleArr = Array.from((await getPuzzle('3')).toUpperCase());
+  console.log(`puzzleArr => ${puzzleArr} `);
 
-  puzzleWordStringStar(puzzle);
-};
-
-// 2. PUZZLE `*`표시
-function puzzleWordStringStar(puzzle) {
-  for (let word of puzzle) {
-    console.log(`word => ${word} `);
+  // 2. PUZZLE `*`표시
+  puzzleArr.forEach((puzzle) => {
     const span = document.createElement('span');
-    if (word !== ' ') {
+    if (puzzle !== ' ') {
       span.innerText = '*';
     } else {
       span.innerText = '';
     }
     puzzleDiv.appendChild(span);
-  }
+  });
 
-  document.addEventListener('keypress', compare);
-}
+  guesses.innerText = `Guesses left: 5`;
 
-// 3. 눌러진 키(KEY)와 서버에서 가져온 PUZZLE 비교하기
-function compare(event, word) {
-  console.log(event.key);
-  if (event.key === word) {
-    span.innerText = `${word}`;
-  }
-}
+  // 3. 눌러진 키(KEY)와 서버에서 가져온 PUZZLE 비교하기
+  document.addEventListener('keypress', (event) => {
+    const spanNum = document.querySelectorAll('span');
+    for (let i = 0; i < puzzleArr.length; i++) {
+      if (puzzleArr[i] === event.key.toUpperCase()) {
+        console.log(`event.key => ${event.key.toUpperCase()} `);
+        spanNum[i].innerText = `${event.key.toUpperCase()}`;
+      }
+    }
 
-// document.addEventListener('keypress', (event) => {
-//   console.log(event.key);
-//   if (event.key === word) {
-//     span.innerText = `${word}`;
-//   }
-// });
+    if (pushCount > 1) {
+      --pushCount;
+      guesses.innerText = `Guesses left: ${pushCount}`;
+    } else {
+      guesses.innerText = `Nice try! The word was "award winning service"`;
+    }
+  });
+};
 
 document.querySelector('#reset').addEventListener('click', startGame);
 startGame();
