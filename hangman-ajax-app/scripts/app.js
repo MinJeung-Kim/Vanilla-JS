a69198af3ef3c35466766b54190f6a9ef56736f7
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-01 20:02:50 +0900

    add reset button function

--- hangman-ajax-app/scripts/app.js
+++ hangman-ajax-app/scripts/app.js
@@ -5,6 +5,9 @@ const guesses = document.querySelector('#guesses');
 let pushCount = 5;
 // 1. PUZZLE 가져오기 (서버연결)
 const startGame = async () => {
+  puzzleDiv.innerHTML = '';
+
+  guesses.innerText = `Guesses left: ${(pushCount = 5)}`;
   const puzzleArr = Array.from((await getPuzzle('3')).toUpperCase());
   console.log(`puzzleArr => ${puzzleArr} `);
 
@@ -19,21 +22,21 @@ const startGame = async () => {
     puzzleDiv.appendChild(span);
   });
 
-  guesses.innerText = `Guesses left: 5`;
-
   // 3. 눌러진 키(KEY)와 서버에서 가져온 PUZZLE 비교하기
   document.addEventListener('keypress', (event) => {
     const spanNum = document.querySelectorAll('span');
-    for (let i = 0; i < puzzleArr.length; i++) {
-      if (puzzleArr[i] === event.key.toUpperCase()) {
-        console.log(`event.key => ${event.key.toUpperCase()} `);
-        spanNum[i].innerText = `${event.key.toUpperCase()}`;
-      }
-    }
-
     if (pushCount > 1) {
-      --pushCount;
-      guesses.innerText = `Guesses left: ${pushCount}`;
+      if (puzzleArr.indexOf(event.key.toUpperCase()) === -1) {
+        --pushCount;
+        guesses.innerText = `Guesses left: ${pushCount}`;
+      } else {
+        for (let i = 0; i < puzzleArr.length; i++) {
+          if (puzzleArr[i] === event.key.toUpperCase()) {
+            console.log(`event.key => ${event.key.toUpperCase()} `);
+            spanNum[i].innerText = `${event.key.toUpperCase()}`;
+          }
+        }
+      }
     } else {
       guesses.innerText = `Nice try! The word was "award winning service"`;
     }
